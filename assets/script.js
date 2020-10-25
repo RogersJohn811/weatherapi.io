566592dcfd1805413939f67a7ae10912272d5ba6
RogersJohn811 <RogersJohn811@gmail.com>
2020-10-24 20:34:33 -0400

    Editng JS

--- assets/script.js
+++ assets/script.js
@@ -200,4 +200,86 @@ $(document).ready(function () {
                     }
                 });
 
-            
+            });//create another ajax call so that the 5day forecast changes when the user clicks a stored city button.
+            $.ajax({
+                method: "GET",
+                url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey
+            }).then(function (fcBtnResponse) {
+                for (var i = 0; i < 8; i++) {
+
+                    if ((fcBtnResponse.list[i].dt_txt).substring(11) === "12:00:00") {
+                        $("#5dayCast").html("<h1>5 Day Forecast</h1>" + "<hr>"); //title
+                        var date1 = [
+                            {
+                                date: ("<p>" + fcBtnResponse.list[i].dt_txt + "</p>"),
+                                temperature: ("<h3>" + "Temperature: " + fcBtnResponse.list[i].main.temp + " F " + "</h3>"),
+                                humidity: ("<h3>" + "Humidity: " + fcBtnResponse.list[i].main.humidity + "</h3>" + "<hr>")
+                            }
+                        ];
+                        var forecastIcon1 = fcBtnResponse.list[0].weather[0].icon;
+                        var fcIcon1 = "http://openweathermap.org/img/wn/" + forecastIcon1 + "@2x.png";
+                        var addforecastIcon1 = $("#5dayCast"); //where the icon is going to go.
+                        var iconImg1 = $("<img>").attr("src", fcIcon1);
+                        addforecastIcon1.append(iconImg1, date1[0].date, date1[0].temperature, date1[0].humidity);
+
+                        var date2 = [
+                            {
+                                date: ("<p>" + fcBtnResponse.list[i + 8].dt_txt + "</p>"),
+                                temperature: ("<h3>" + "Temperature: " + fcBtnResponse.list[i + 8].main.temp + " F " + "</h3>"),
+                                humidity: ("<h3>" + "Humidity: " + fcBtnResponse.list[i + 8].main.humidity + "</h3>" + "<hr>")
+                            }
+                        ];
+                        var forecastIcon2 = fcBtnResponse.list[i + 8].weather[0].icon;
+                        var fcIcon2 = "http://openweathermap.org/img/wn/" + forecastIcon2 + "@2x.png";
+                        var addforecastIcon2 = $("#5dayCast"); //where the icon is going to go.
+                        var iconImg2 = $("<img>").attr("src", fcIcon2);
+                        addforecastIcon2.append(iconImg2, date2[0].date, date2[0].temperature, date2[0].humidity);
+
+                        var date3 = [
+                            {
+                                date: ("<p>" + fcBtnResponse.list[i + 16].dt_txt + "</p>"),
+                                temperature: ("<h3>" + "Temperature: " + fcBtnResponse.list[i + 16].main.temp + " F " + "</h3>"),
+                                humidity: ("<h3>" + "Humidity: " + fcBtnResponse.list[i + 16].main.humidity + "<h3>" + "<hr>")
+                            }
+                        ];
+                        var forecastIcon3 = fcBtnResponse.list[i + 16].weather[0].icon;
+                        var fcIcon3 = "http://openweathermap.org/img/wn/" + forecastIcon3 + "@2x.png";
+                        var addforecastIcon3 = $("#5dayCast"); //where the icon is going to go.
+                        var iconImg3 = $("<img>").attr("src", fcIcon3);
+                        addforecastIcon3.append(iconImg3, date3[0].date, date3[0].temperature, date3[0].humidity);
+
+                        var date4 = [
+                            {
+                                date: ("<p>" + fcBtnResponse.list[i + 24].dt_txt + "</p>"),
+                                temperature: ("<h3>" + "Temperature: " + fcBtnResponse.list[i + 24].main.temp + " F " + "</h3>"),
+                                humidity: ("<h3>" + "Humidity: " + fcBtnResponse.list[i + 24].main.humidity + "</h3>" + "<hr>")
+                            }
+                        ];
+                        var forecastIcon4 = fcBtnResponse.list[i + 24].weather[0].icon;
+                        var fcIcon4 = "http://openweathermap.org/img/wn/" + forecastIcon4 + "@2x.png";
+                        var addforecastIcon4 = $("#5dayCast"); //where the icon is going to go.
+                        var iconImg4 = $("<img>").attr("src", fcIcon4);
+                        addforecastIcon4.append(iconImg4, date4[0].date, date4[0].temperature, date4[0].humidity);
+
+                        var date5 = [
+                            {
+                                date: ("<p>" + fcBtnResponse.list[i + 32].dt_txt + "</p>"),
+                                temperature: ("<h3>" + "Temperature: " + fcBtnResponse.list[i + 32].main.temp + " F " + "</h3>"),
+                                humidity: ("<h3>" + "Humidity: " + fcBtnResponse.list[i + 32].main.humidity + "</h3>")
+                            }
+                        ];
+                        var forecastIcon5 = fcBtnResponse.list[i + 32].weather[0].icon; //icon
+                        var fcIcon5 = "http://openweathermap.org/img/wn/" + forecastIcon5 + "@2x.png"; //grabbing icon
+                        var addforecastIcon5 = $("#5dayCast");
+                        var iconImg5 = $("<img>").attr("src", fcIcon5);
+                        addforecastIcon5.append(iconImg5, date5[0].date, date5[0].temperature, date5[0].humidity);
+                    }
+                }
+
+            });
+        };
+        renderCities();
+        // Adding click event listeners to all elements with a id of "cityList" aka the city blocks that will be clicked on.
+        $(document).on("click", "#cityList", displayCityInfo);
+    });
+});
